6cf79ddcd77ac3401a61f6cdaf98d389ae173351
Tjidde <tjidde@tjidde.nl>
2025-05-29 10:41:43 +0200

    Update ExportActionBean.java

--- src/main/java/nl/b3p/kar/stripes/ExportActionBean.java
+++ src/main/java/nl/b3p/kar/stripes/ExportActionBean.java
@@ -653,7 +653,7 @@ public class ExportActionBean implements ActionBean, ValidationErrorHandler {
 
     public Resolution rseqByKarAddress() throws JSONException {
         EntityManager em = Stripersist.getEntityManager();
-        log.log( "Got into rseqByKarAddress");
+        log.error( "Got into rseqByKarAddress");
         System.err.print("Got into rseqByKarAddress");
         JSONObject info = new JSONObject();
         info.put("success", Boolean.FALSE);
@@ -680,7 +680,7 @@ public class ExportActionBean implements ActionBean, ValidationErrorHandler {
 
     public Resolution rseqByDataowner() throws JSONException {
         EntityManager em = Stripersist.getEntityManager();
-                log.log( "Got into rseqByDataowner");
+                log.error( "Got into rseqByDataowner");
         System.err.print("Got into rseqByDataowner");
         JSONObject info = new JSONObject();
         info.put("success", Boolean.FALSE);
@@ -708,7 +708,7 @@ public class ExportActionBean implements ActionBean, ValidationErrorHandler {
 
     public Resolution allRseqs() throws JSONException {
         EntityManager em = Stripersist.getEntityManager();
-                log.log( "Got into allRseqs");
+                log.error( "Got into allRseqs");
         System.err.print("Got into allRseqs");
         JSONObject info = new JSONObject();
         info.put("success", Boolean.FALSE);
@@ -749,21 +749,21 @@ public class ExportActionBean implements ActionBean, ValidationErrorHandler {
 
     private JSONArray makeRseqArray(List<RoadsideEquipment> rseqs) throws JSONException {
         JSONArray rseqArray = new JSONArray();
-        log.log( "Amount of rseqs"+rseqs.size());
+        log.error( "Amount of rseqs"+rseqs.size());
         System.err.print("Amount of rseqs"+rseqs.size());
         for (RoadsideEquipment rseqObj : rseqs) {
             
         
             if (getGebruiker().canRead(rseqObj)) {
                 if (onlyValid && !rseqObj.isValid()) {
-                      log.log( "NOT VALID"+rseqObj.getId());
+                      log.error( "NOT VALID"+rseqObj.getId());
                       System.err.print("RESQOBJ NOT VALID"+rseqObj.getId());
                     continue;
                 }
                 
                 if(vehicleType == null || vehicleType.equalsIgnoreCase(VehicleType.VEHICLE_TYPE_GEMIXT) || rseqObj.getVehicleType().equalsIgnoreCase(vehicleType) ||  rseqObj.getVehicleType().equalsIgnoreCase(VehicleType.VEHICLE_TYPE_GEMIXT)){
                   System.err.print("Got into IF");
-                log.log( "Got into IF");
+                log.error( "Got into IF");
                 JSONObject jRseq = new JSONObject();
                     jRseq.put("id", rseqObj.getId());
                     jRseq.put("naam", rseqObj.getDescription());
@@ -787,7 +787,7 @@ public class ExportActionBean implements ActionBean, ValidationErrorHandler {
     @Override
     public Resolution handleValidationErrors(ValidationErrors errors) throws Exception {
         System.err.print("Got into handleValidationErrors");
-                log.log( "Got into handleValidationErrors");
+                log.error( "Got into handleValidationErrors");
         return null;
     }
 
